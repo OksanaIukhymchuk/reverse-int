4272a8033cfcbd9256ab4ab0acc57eab01438676
OksanaIukhymchuk <66162022+OksanaIukhymchuk@users.noreply.github.com>
2022-01-07 12:30:17 +0200

    fix: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let reverseNum = '';
+    let absoluteNumberToString = Math.abs(n).toString();
+    for (let i = absoluteNumberToString.length-1; i>=0; i--) {
+        reverseNum += absoluteNumberToString[i];
+    }
+    return Number(reverseNum);
 }
